9cfd974c56e7e30f8332e83822c69a1918151b9c
леся ким <145535976+riloqq@users.noreply.github.com>
2024-02-29 14:15:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 class deBuLa3:
-    def __init__(self, debul1, debul2, debul3, debul4):
+    def __init__(self, debul1, debul2, debul3, umni1):
         self.debul1 = debul1
         self.debul2 = debul2
         self.debul3 = debul3
-        self.debul4 = debul4
+        self.umni1 = umni1
 
     def __str__(self):
-        return f"debul1: {self.debul1}\ndebul2: {self.debul2}\ndebul3: {self.debul3}\ndebul4: {self.debul4}"
+        return f"debul1: {self.debul1}\ndebul2: {self.debul2}\ndebul3: {self.debul3}\numni1: {self.umni1}"
 
 
 OutCommand = deBuLa3("Мегачипс", "Dimooon", "matveeeeeeey", "Olesya")
