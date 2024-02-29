6ecd9add520c2aaa2594d81c43b3a378a92cfb1d
Ecodrin <matvfo@ya.ru>
2024-02-29 14:01:41 +0300

    new branch

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 class deBuLa3:
-    def __init__(self, debul1, debul2, debul3):
+    def __init__(self, debul1, debul2, debul3, debul4):
         self.debul1 = debul1
-        self.debul2 = debul1
-        self.debul3 = debul1
+        self.debul2 = debul2
+        self.debul3 = debul3
+        self.debul4 = debul4
+
+    def __str__(self):
+        return f"debul1: {self.debul1}\ndebul2: {self.debul2}\ndebul3: {self.debul3}\ndebul4: {self.debul4}"
+
+
+OutCommand = deBuLa3("Мегачипс", "Dimooon", "matveeeeeeey", "Olesya")
+print(OutCommand)
